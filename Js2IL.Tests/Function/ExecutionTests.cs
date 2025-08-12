17979020ae72660d9e80f0a587d8a19756c7ccc6
Tom Cox <tom_a_cox@hotmail.com>
2025-08-11 21:44:20 -0700

    feat(function): support functions with up to 6 parameters and add tests

--- Js2IL.Tests/Function/ExecutionTests.cs
+++ Js2IL.Tests/Function/ExecutionTests.cs
@@ -18,6 +18,9 @@ namespace Js2IL.Tests.Function
         [Fact]
         public Task Function_GlobalFunctionWithParameter() { var testName = nameof(Function_GlobalFunctionWithParameter); return ExecutionTest(testName); }
 
+    [Fact]
+    public Task Function_GlobalFunctionWithMultipleParameters() { var testName = nameof(Function_GlobalFunctionWithMultipleParameters); return ExecutionTest(testName); }
+
         [Fact]
         public Task Function_ReturnsStaticValueAndLogs() { var testName = nameof(Function_ReturnsStaticValueAndLogs); return ExecutionTest(testName); }
 
--- Js2IL.Tests/Function/GeneratorTests.cs
+++ Js2IL.Tests/Function/GeneratorTests.cs
@@ -24,6 +24,9 @@ namespace Js2IL.Tests.Function
         [Fact]
         public Task Function_GlobalFunctionWithParameter() { var testName = nameof(Function_GlobalFunctionWithParameter); return GenerateTest(testName); }
 
+    [Fact]
+    public Task Function_GlobalFunctionWithMultipleParameters() { var testName = nameof(Function_GlobalFunctionWithMultipleParameters); return GenerateTest(testName); }
+
         [Fact]
         public Task Function_ReturnsStaticValueAndLogs() { var testName = nameof(Function_ReturnsStaticValueAndLogs); return GenerateTest(testName); }
 
