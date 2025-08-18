6bc878d56ee1acbe41136764316ed8708f6872ef
Tom Cox <tom_a_cox@hotmail.com>
2025-08-18 12:38:01 -0700

    tests(TryCatch): fix GeneratorTests to call GenerateTest, not ExecutionTest

--- Js2IL.Tests/TryCatch/GeneratorTests.cs
+++ Js2IL.Tests/TryCatch/GeneratorTests.cs
@@ -9,5 +9,7 @@ namespace Js2IL.Tests.TryCatch
         }
 
         // Add try/catch generator tests here
+    [Fact]
+    public Task TryCatch_NoBinding() { var testName = nameof(TryCatch_NoBinding); return GenerateTest(testName); }
     }
 }
