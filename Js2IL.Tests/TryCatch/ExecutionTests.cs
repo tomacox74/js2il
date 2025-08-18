ef1bb3963f576b6127be14e945bb926a013775c2
Tom Cox <tom_a_cox@hotmail.com>
2025-08-18 12:05:00 -0700

    tests(TryCatch): add subgroup with empty Execution and Generator test classes

---
+++ Js2IL.Tests/TryCatch/ExecutionTests.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace Js2IL.Tests.TryCatch
+{
+    public class ExecutionTests : ExecutionTestsBase
+    {
+        public ExecutionTests() : base("TryCatch")
+        {
+        }
+
+        // Add try/catch execution tests here
+    }
+}
---
+++ Js2IL.Tests/TryCatch/GeneratorTests.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace Js2IL.Tests.TryCatch
+{
+    public class GeneratorTests : GeneratorTestsBase
+    {
+        public GeneratorTests() : base("TryCatch")
+        {
+        }
+
+        // Add try/catch generator tests here
+    }
+}
