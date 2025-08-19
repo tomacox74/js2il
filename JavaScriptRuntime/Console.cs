eb2211772e01385198b841ce4b82956161f4e3cf
Tom Cox <tom_a_cox@hotmail.com>
2025-08-19 12:01:43 -0700

    Runtime: add IntrinsicObjectAttribute and annotate Console as [IntrinsicObject(" console\)]

--- JavaScriptRuntime/Console.cs
+++ JavaScriptRuntime/Console.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace JavaScriptRuntime
 {
+    [IntrinsicObject("console")]
     public class Console
     {
         private static IConsoleOutput _output = new DefaultConsoleOutput();
