88a1057ae41d8ae060aa6f71234e9c92a574735d
Tom Cox <tom_a_cox@hotmail.com>
2025-09-02 14:43:03 +0300

    misc

--- Js2IL.Tests/ConsoleTests.cs
+++ Js2IL.Tests/ConsoleTests.cs
@@ -22,7 +22,7 @@ namespace JavaScriptRuntime.Tests
             var testOutput = new TestConsoleOutput();
             Console.SetOutput(testOutput);
 
-            Console.Log("Hello", "World", 42, null);
+            Console.Log("Hello", "World", 42, JavaScriptRuntime.JsNull.Null);
 
             Assert.Single(testOutput.Output);
             Assert.Equal("Hello World 42 null", testOutput.Output[0]);
