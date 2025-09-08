18f83d278cc42c4bae36d388ed9e287cb1f41146
Tom Cox <tom_a_cox@hotmail.com>
2025-09-08 03:51:23 +0300

    Added new test to speficially check for a bug

--- Js2IL.Tests/Array/GeneratorTests.cs
+++ Js2IL.Tests/Array/GeneratorTests.cs
@@ -42,7 +42,10 @@ namespace Js2IL.Tests.Array
         [Fact]
         public Task Array_Splice_InsertAndDelete() { var testName = nameof(Array_Splice_InsertAndDelete); return GenerateTest(testName); }
 
-    [Fact]
-    public Task Array_New_Empty() { var testName = nameof(Array_New_Empty); return GenerateTest(testName); }
+        [Fact]
+        public Task Array_New_Empty() { var testName = nameof(Array_New_Empty); return GenerateTest(testName); }
+
+        [Fact]
+        public Task Array_AsArray_Ternary() { var testName = nameof(Array_AsArray_Ternary); return GenerateTest(testName); }
     }
 }
