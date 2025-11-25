7737062a78d6d218132c32ea74f1c7c945edb051
Tom Cox <tom_a_cox@hotmail.com>
2025-11-24 21:37:53 -0800

    test: Add failing test for class constructor instantiating class with scope references
    Reproduces bug where a class constructor that doesn't need parent scopes
    tries to instantiate another class that does need parent scopes, causing
    InvalidOperationException due to missing _scopes field.
    
    Test case: Outer class constructor creates Inner class instance, where
    Inner references GLOBAL_VALUE constant from parent scope.
    
    Added diagnostic Console.WriteLine statements in ILExpressionGenerator to
    help diagnose the scope array construction issue.

---
+++ Js2IL.Tests/Classes/JavaScript/Classes_ClassConstructor_NewClassReferencingGlobal.js
@@ -0,0 +1,20 @@
+// Test: Class constructor instantiates another class that references a global variable
+// This reproduces the bug where the constructor scope array construction fails
+// when a nested class needs to access parent scopes.
+
+const GLOBAL_VALUE = 42;
+
+class Inner {
+    constructor() {
+        this.value = GLOBAL_VALUE;
+    }
+}
+
+class Outer {
+    constructor() {
+        this.inner = new Inner();
+    }
+}
+
+const obj = new Outer();
+console.log(obj.inner.value);
