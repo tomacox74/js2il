409a940074592c118aacec7defa46bf5c3f18924
Tom Cox <tom_a_cox@hotmail.com>
2025-11-26 21:13:27 -0800

    console: format arrays in console.log to match Node ([ 1, 2 ]) and add test + snapshots

---
+++ Js2IL.Tests/Array/JavaScript/Array_ConsoleLog_PrintsArrayContent.js
@@ -0,0 +1,4 @@
+// Test that console.log properly stringifies array content instead of printing type name
+const primes = [2, 3, 5, 7, 11, 13];
+console.log('Array contents:', primes);
+console.log(primes);
