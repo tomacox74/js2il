06538be35c51b3732c7be99efb560d3db2575913
Tom Cox <tom_a_cox@hotmail.com>
2025-12-13 15:30:34 -0800

    style(tests): sort Promise GeneratorTests methods alphabetically

--- Js2IL.Tests/Promise/GeneratorTests.cs
+++ Js2IL.Tests/Promise/GeneratorTests.cs
@@ -11,61 +11,6 @@ public class GeneratorTests : GeneratorTestsBase
     {
         return GenerateTest(nameof(Promise_Executor_Resolved));
     }
-
-    [Fact]
-    public Task Promise_Executor_Rejected()
-    {
-        return GenerateTest(nameof(Promise_Executor_Resolved));
-    }
-
-    [Fact]
-    public Task Promise_Resolve_Then()
-    {
-        return GenerateTest(nameof(Promise_Resolve_Then));
-    }
-
-    [Fact]
-    public Task Promise_Reject_Then()
-    {
-        return GenerateTest(nameof(Promise_Reject_Then));
-    }
-
-    [Fact]
-    public Task Promise_Resolve_ThenFinally()
-    {
-        return GenerateTest(nameof(Promise_Resolve_ThenFinally));
-    }
-
-    [Fact]
-    public Task Promise_Reject_FinallyCatch()
-    {
-        return GenerateTest(nameof(Promise_Reject_FinallyCatch));
-    }
-
-    [Fact]
-    public Task Promise_Resolve_FinallyThen()
-    {
-        return GenerateTest(nameof(Promise_Resolve_FinallyThen));
-    }
-
-    [Fact]
-    public Task Promise_Resolve_FinallyThrows()
-    {
-        return GenerateTest(nameof(Promise_Resolve_FinallyThrows));
-    }
-
-    [Fact]
-    public Task Promise_Then_ReturnsResolvedPromise()
-    {
-        return GenerateTest(nameof(Promise_Then_ReturnsResolvedPromise));
-    }
-
-    [Fact]
-    public Task Promise_Then_ReturnsRejectedPromise()
-    {
-        return GenerateTest(nameof(Promise_Then_ReturnsRejectedPromise));
-    }
-
     [Fact]
     public Task Promise_Catch_ReturnsResolvedPromise()
     {
@@ -76,6 +21,18 @@ public class GeneratorTests : GeneratorTestsBase
     public Task Promise_Catch_ReturnsRejectedPromise()
     {
         return GenerateTest(nameof(Promise_Catch_ReturnsRejectedPromise));
+    }
+
+    [Fact]
+    public Task Promise_Executor_Resolved()
+    {
+        return GenerateTest(nameof(Promise_Executor_Resolved));
+    }
+
+    [Fact]
+    public Task Promise_Executor_Rejected()
+    {
+        return GenerateTest(nameof(Promise_Executor_Resolved));
     }
 
     [Fact]
@@ -91,8 +48,56 @@ public class GeneratorTests : GeneratorTestsBase
     }
 
     [Fact]
+    public Task Promise_Reject_FinallyCatch()
+    {
+        return GenerateTest(nameof(Promise_Reject_FinallyCatch));
+    }
+
+    [Fact]
+    public Task Promise_Reject_Then()
+    {
+        return GenerateTest(nameof(Promise_Reject_Then));
+    }
+
+    [Fact]
+    public Task Promise_Resolve_FinallyThen()
+    {
+        return GenerateTest(nameof(Promise_Resolve_FinallyThen));
+    }
+
+    [Fact]
+    public Task Promise_Resolve_FinallyThrows()
+    {
+        return GenerateTest(nameof(Promise_Resolve_FinallyThrows));
+    }
+
+    [Fact]
+    public Task Promise_Resolve_Then()
+    {
+        return GenerateTest(nameof(Promise_Resolve_Then));
+    }
+
+    [Fact]
+    public Task Promise_Resolve_ThenFinally()
+    {
+        return GenerateTest(nameof(Promise_Resolve_ThenFinally));
+    }
+
+    [Fact]
     public Task Promise_Scheduling_StarvationTest()
     {
         return GenerateTest(nameof(Promise_Scheduling_StarvationTest));
+    }
+
+    [Fact]
+    public Task Promise_Then_ReturnsResolvedPromise()
+    {
+        return GenerateTest(nameof(Promise_Then_ReturnsResolvedPromise));
+    }
+
+    [Fact]
+    public Task Promise_Then_ReturnsRejectedPromise()
+    {
+        return GenerateTest(nameof(Promise_Then_ReturnsRejectedPromise));
     }
 }
