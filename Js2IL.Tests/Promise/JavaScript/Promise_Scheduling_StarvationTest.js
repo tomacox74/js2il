3bc2d3cc3120bbf66d8d6c8c1e831bdc16f5bd86
Tom Cox <tom_a_cox@hotmail.com>
2025-12-13 15:39:34 -0800

    correct starvation test and misc snapshot updates

--- Js2IL.Tests/Promise/JavaScript/Promise_Scheduling_StarvationTest.js
+++ Js2IL.Tests/Promise/JavaScript/Promise_Scheduling_StarvationTest.js
@@ -1,15 +1,18 @@
 // the maximum number of microtasks that can execute sequentially is 1024 before the next macrotask is executed
 // we test this by enquening 2048 promises and 1 setTimer
-console.log("Invoking setTimer");
-setTimeout(() => {
-    console.log("setTimer executed");
-}, 0);
-
 console.log("Creating 2048 Promises")
 for (let i = 0; i < 2048; i++) {
     Promise.resolve(i).then((id) => { 
+        if (id === 512)
+        {
+            console.log("[then] scheduling a timer at promise number:", id);
+            setTimeout(() => {
+                console.log("setTimer executed");
+            }, 0);
+        }
+
         // logging output from all the promises would make the test slow
-        if (id === 1024 || id === 2047) {
+        if (id === 1023 || id === 2047) {
             console.log("executed promise number:", id);
         }
     });
